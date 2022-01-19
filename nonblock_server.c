18d5758bcc485550b99568cbc18ed0bdba939346
shkwon98 <86837107+shkwon98@users.noreply.github.com>
2022-01-19 14:53:10 +0900

    Add files via upload

---
+++ nonblock_server.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
+#define PORT 2000
+
+void errorHandling(char *errmsg);
+
+void another_task();
+
+int main() {
+    char buf[BUF_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    int client_addr_size = sizeof(client_addr);
+    int server_sock, client_sock;
+
+    if( (server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 )
+        errorHandling("socket() error");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+
+    if( bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0 )
+        errorHandling("bind() error");
+
+    if( listen(server_sock, 5) < 0 )
+        errorHandling("listen() error");
+
+    printf("----- accept wait\n");
+
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_size);
+    if( client_sock < 0 )
+        errorHandling("accept() error");
+
+    printf("----- accept client\n");
+
+    int flag = fcntl(client_sock, F_GETFL, 0);
+    fcntl(client_sock, F_SETFL, flag | O_NONBLOCK);
+
+    int read_rtn;
+    while(1) {
+        sleep(1);
+
+        printf("----- read wait\n");
+
+        memset(buf, 0x00, sizeof(buf));
+        read_rtn = read(client_sock, buf, sizeof(buf));
+
+        if ( read_rtn < 0 ) {
+            if(errno == EAGAIN) {
+                printf("----- read EAGAIN\n");
+            }
+            else {
+                printf("----- read error\n");
+                printf("----- Socket close\n");
+                close(client_sock);
+                break;
+            }
+        }
+        else if( read_rtn == 0 ) {
+            printf("----- Socket close\n");
+            close(client_sock);
+            break;
+        }
+        else {
+            printf("----- read : %s\n", buf);
+        }
+
+        another_task();
+    }
+
+    return 0;
+
+}
+
+void another_task() {
+        printf("----- Another task processing...\n");
+}
+
+void errorHandling(char* errmsg) {
+    fputs(errmsg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}   
